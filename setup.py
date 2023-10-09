137e458725eaf3cacfb12a27160c81b402c231ad
Shadab Ahmed <ahmed11shadab@gmail.com>
2023-10-09 22:23:11 +0530

    Data Ingestion

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='mlproject',
     version='0.0.1',
-    author='Krish',
-    author_email='krishnaik06@gmail.com',
+    author='shadab',
+    author_email='ahmed11shadab@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
 )
